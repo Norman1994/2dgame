87ac9e10b063bf22e09e870832202c4f075dd9f1
Norman1994 <kazin-dima@mail.ru>
2020-09-28 21:16:02 +0300

    Release save game

--- Assets/Scripts/SaveGameManager.cs
+++ Assets/Scripts/SaveGameManager.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class SaveGameManager : MonoBehaviour
 {
     public static SaveGameManager instance;
     public static string persistentDataPath;
-    //public Keybindings keys;
-    // Start is called before the first frame update
+    public Character character;
+
+    BinaryFormatter bf = new BinaryFormatter();
 
     void Start()
 	{
@@ -33,6 +35,30 @@ public class SaveGameManager : MonoBehaviour
         if (!IsSaveFile())
         {
             Directory.CreateDirectory(Application.persistentDataPath + "/game_save");
+        }
+        if (!Directory.Exists(Application.persistentDataPath + "/game_save/character_status"))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/game_save/character_status");
+        }
+
+        FileStream file = File.Create(Application.persistentDataPath + "/game_save/character_status/save.txt");
+
+        var json = JsonUtility.ToJson(character);
+        bf.Serialize(file, json);
+        file.Close();
+    }
+
+    public void LoadGame()
+    {
+        if (!Directory.Exists(Application.persistentDataPath + "/game_save/character_status"))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/game_save/character_status");
+        }
+        if (File.Exists(Application.persistentDataPath + "/game_save/character_status/save.txt"))
+        {
+            FileStream file = File.Open(Application.persistentDataPath + "/game_save/character_status/save.txt", FileMode.Open);
+            JsonUtility.FromJsonOverwrite((string)bf.Deserialize(file), character);
+            file.Close();
         }
     }
 }
